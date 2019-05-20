30c67ca2222f0d10192a149fc4530af82d42b061
Guilherme <guilherme.santos97@outlook.com.br>
2019-05-20 00:04:15 -0300

    Inicialização Training-bank
    Inicialização

--- StudentRecordManagementSystem/StudentRecordManagementSystem/Utility/ConnectionString.cs
+++ StudentRecordManagementSystem/StudentRecordManagementSystem/Utility/ConnectionString.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace StudentRecordManagementSystem.Utility
+﻿namespace StudentRecordManagementSystem.Utility
 {
     public static class ConnectionString
     {
-        private static string connectionName = "Data Source=;Initial Catalog=;User ID=;Password=;";
+        private static string connectionName = "Data Source=172.18.32.1;Initial Catalog=CrudCoreAdo;User ID=sa;Password=Dev*2019;";
 
         public static string CName
         {
